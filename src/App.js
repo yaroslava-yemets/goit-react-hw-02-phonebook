7342f1569a67434fc483b180a6e94671b17a6cfc
Yaroslava Yemets <yaroslava1058@gmail.com>
2021-08-22 15:29:50 +0300

    added inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleNameChange = (evt) => {
+    this.setState({
+      name: evt.currentTarget.value,
+    });
+  };
+
+  handleNumberChange = (evt) => {
+    this.setState({
+      number: evt.currentTarget.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <form>
+          <label>
+          Name
+            <input
+              value={this.state.name}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+              onChange={this.handleNameChange}
+            />
+          </label>
+
+          <label>
+            Number
+            <input
+              value={this.state.number}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
+              onChange={this.handleNumberChange}
+            />
+          </label>
+          <button
+            type="button"
+
+          >
+            Add contact
+          </button>
+
+        </form>
+
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            <li>Yaroslava</li>
+          </ul>
+        </div>
+      </div>
+    )}
 }
 
 export default App;
